c8a855346eba3d6567364236ae35a6db62f45d5b
tra vo huynh anh <travohuynhanhbc1@gmail.com>
2026-08-21 16:25:19 +0700

    ex03

---
+++ cslt/session3/ex03.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cslt.session3
+{
+    internal class ex03
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input celsius: ");
+
+            if (double.TryParse(Console.ReadLine(), out double celsius))
+            {
+                double kelvin = celsius + 273;
+                double fahrenheit = (celsius * 18 / 10) + 32;
+
+                Console.WriteLine($"kelvin = {kelvin}");
+                Console.WriteLine($"fahrenheit = {fahrenheit}");
+            }
+            else
+            { Console.WriteLine("incorrect, please try again "); }
+        }
+    }
+}
