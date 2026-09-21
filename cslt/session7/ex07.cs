47bb9fb780ed036f421a37b1b050dcee1e05cfad
travohuynhanh <travohuynhanhbc1@gmail.com>
2026-09-21 16:20:08 +0700

    update ex07

---
+++ cslt/session7/ex07.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cslt.session7
+{
+    internal class ex07
+    {
+        public static void main()
+        {
+            Console.WriteLine("Nhap so phan tu cua mang: ");
+            int[] arr = {1,2,3,4};
+        }
+        static double dtb(int[] arr);
+
+    }
+}
